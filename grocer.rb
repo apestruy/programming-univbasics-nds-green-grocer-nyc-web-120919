0d9cb8db85ac37b10418660c8c3d7ea5ae416b79
Alexandra Pestruyeva <apestruy@gmail.com>
2019-12-04 03:52:28 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -43,21 +43,21 @@ con_cart = []
 end 
 
 def apply_coupons(cart, coupons)
-  result = {}
-  cart.each do |food, info|
-    coupons.each do |coupon|
-      if food == coupon[:item] && info[:count] >= coupon[:num]
-        info[:count] =  info[:count] - coupon[:num]
-        if result["#{food} W/COUPON"]
-          result["#{food} W/COUPON"][:count] += 1
-        else
-          result["#{food} W/COUPON"] = {:price => coupon[:cost], :clearance => info[:clearance], :count => 1}
-        end
-      end
+ new_array = []
+  for item in cart do
+    coupon = get_coupon(item, coupons)
+    if coupon
+      discounted_price = coupon[:cost] / coupon[:num]
+      discounted_item = {:item => item[:item] + " W/COUPON", :price => discounted_price,
+        :clearance => item[:clearance], :count => coupon[:num]}
+      new_array.push(discounted_item)
+      no_sale_item = {:item => item[:item],:price => item[:price], :clearance => item[:clearance], :count => item[:count] - coupon[:num]}
+        new_array.push(no_sale_item)
+    else
+      new_array.push(item)
     end
-    result[food] = info
   end
-  result
+  new_array
 end
 
 def apply_clearance(cart)
