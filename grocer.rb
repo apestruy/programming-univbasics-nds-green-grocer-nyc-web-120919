e8c584727da92659ded429a2b2ec6a2d5765f5b0
Alexandra Pestruyeva <apestruy@gmail.com>
2019-12-04 03:02:46 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -43,9 +43,7 @@ con_cart = []
 end 
 
 def apply_coupons(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+  
 end
 
 def apply_clearance(cart)
