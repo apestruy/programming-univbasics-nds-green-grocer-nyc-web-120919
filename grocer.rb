eb69f7dae493dc59cb86e7911799a78845d82334
Alexandra Pestruyeva <apestruy@gmail.com>
2019-12-04 04:25:29 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -44,12 +44,11 @@ end
 
 
 def apply_coupons(cart, coupons)
-  index1 = 0 
-  index2 = 0 
+  index1 = 0
   cart_with_coupons = cart
   while index1 < cart.length do 
     index2 = 0 
-      while index2< coupons.length do
+      while index2 < coupons.length do
         if cart[index1][:item] == coupons[index2][:item]
          while cart[index1][:count] >= coupons[index2][:num] do
            cart[index1][:count] -= coupons[index2][:num]
