28799e7fee49ef9dabb7c3d6f028df72ff2e44f3
Alexandra Pestruyeva <apestruy@gmail.com>
2019-12-04 03:29:14 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -43,10 +43,19 @@ con_cart = []
 end 
 
 def apply_coupons(cart, coupons)
-  coupon_array = []
-  index = 0 
-  while index < cart.length do
-    if cart[]
+  coupons.each do |coupon|
+    name = coupon[:item]
+    if cart[name] && cart[name][:count] >= coupon[:num]
+      if cart["#{name} W/COUPON"]
+        cart["#{name} W/COUPON"][:count] += 1
+      else
+        cart["#{name} W/COUPON"] = {:count => 1, :price => coupon[:cost]}
+        cart["#{name} W/COUPON"][:clearance] = cart[name][:clearance]
+      end
+      cart[name][:count] -= coupon[:num]
+    end
+  end
+  cart
 end
 
 def apply_clearance(cart)
