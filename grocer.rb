a9ea96ea15d89c61f661c62da9a6a12ba1b78820
Alexandra Pestruyeva <apestruy@gmail.com>
2019-12-04 04:30:19 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -66,20 +66,8 @@ def apply_coupons(cart, coupons)
   cart_with_coupons
 end
 
- 
- 
- 
- 
-
 def apply_clearance(cart)
-  cart.each do |item, attribute_hash| 
-    if attribute_hash[:clearance] == true 
-      attribute_hash[:price] = (attribute_hash[:price] *
-      0.8).round(2) 
-    end 
-  end 
-cart 
-end
+ 
 
 def checkout(cart, coupons)
   # Consult README for inputs and outputs
